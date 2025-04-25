0bbb6f821f1d1bf6f97e48939302cb93a9b81cff
aoeu128 <aoeu128@gmail.com>
2025-04-24 23:44:03 -0400

    portfolio v2

---
+++ src/content_option.js
@@ -0,0 +1,191 @@
+import alethea from "./assets/images/alethea.png";
+import nobleblocks from "./assets/images/nobleblocks.png";
+import gg from "./assets/images/gg.png";
+import lowden from "./assets/images/lowden.png";
+import nerdbunny from "./assets/images/nerdbunny.png";
+import qube from "./assets/images/qube.png";
+import flip from "./assets/images/flip.png";
+import alpha from "./assets/images/alpha.png";
+import ai from "./assets/images/ai.png";
+const logotext = "David";
+const meta = {
+  title: "David Kuczynski",
+  description:
+    "I’m David Kuczynski AI, Blockchain Developer _ Full stack devloper",
+};
+
+const introdata = {
+  title: "I’m David Kuczynski",
+  animated: {
+    first: "AI, Blockchain Expert",
+    second: "I provide AI, Blockchain Services",
+    third: "I develop cool websites",
+  },
+  description: "Seasoned Full Stack Developer, AI,Blockchain Developer",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "About Myself",
+  aboutme:
+    "Full Stack developer specializing in modern technologies, including AI and Blockchain. Experienced in building blockchain dApps, AI/ML modeling, and website development. Passionate about humor and a soccer enthusiast, eager to contribute to world-class projects and companies.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Full Stack Blockchain Developer",
+    where: "Alethea AI",
+    date: "2024-2025",
+  },
+  {
+    jobtitle: "Full Stack Blockchain/AI Developer",
+    where: "Geniusee",
+    date: "2023-2024",
+  },
+  {
+    jobtitle: "Full Stack Blockchain Developer",
+    where: "Flip Games Ltd",
+    date: "2021-2023",
+  },
+  {
+    jobtitle: "Full Stack Developer",
+    where: "Honeycomb Software",
+    date: "2017-2020",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Rust",
+    value: 90,
+  },
+  {
+    name: "Solidity",
+    value: 90,
+  },
+  {
+    name: "Node.js",
+    value: 90,
+  },
+  {
+    name: "TypeScript",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 90,
+  },
+  {
+    name: "AWS",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "Blochain",
+    description:
+      "I provide service in blockchain including web3 dapp development based on Ethereum and Solana, smart contract development",
+  },
+  {
+    title: "AI",
+    description:
+      "I provide custom AI agent development, chatbot development, ML model building, NLP integration service",
+  },
+  {
+    title: "Website",
+    description:
+      "I provide Cool, Engaging, User friendly Website and Platform development Service",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: alethea,
+    description:
+      "Solana based memecoin trading platform, AI-news platform. Worked as full stack developer, smart contract developer",
+    link: "#",
+  },
+  {
+    img: nobleblocks,
+    description:
+      "Decentralized Publishing platform, Worked as full stack developer, smart contract developer, raised $2M in ICO for the platform",
+    link: "#",
+  },
+  {
+    img: gg,
+    description:
+      "GameFied 3D Game Platform, Worked as Smart contract developer, Backend developer. Deployed Governance token to Solana net",
+    link: "#",
+  },
+  {
+    img: lowden,
+    description:
+      "Invest Advisory Platform, Worked as Full stack developer, developed discord bot, NFT verification system. Listed 5000 NFTs on Opensea",
+    link: "#",
+  },
+  {
+    img: nerdbunny,
+    description:
+      "AI article review site, Worked as Full stack developer, integrated chatgpt to the site, provided seamless AI based article review service",
+    link: "#",
+  },
+  {
+    img: qube,
+    description:
+      "Solana staking node Website. Worked in early stage start up, as ICO mentor, developed staking contract for the validator",
+    link: "#",
+  },
+
+  {
+    img: flip,
+    description:
+      "NFT lootboxes based on Solana chain, Worked as Full stack developer, developed frontend and backend for the NFT purchase and open feature",
+    link: "#",
+  },
+  {
+    img: alpha,
+    description:
+      "NFT marketplace, Worked as Full stack developer, Built more than 10 page UI, developed NFT sale backend feature.",
+    link: "#",
+  },
+  {
+    img: ai,
+    description:
+      "Side project to prove Sutton-Barto's reinforce learning model. Generated supervised model graph, pick, collect and utilized sample data for the proof",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "davidkucynski0531@gmail.com",
+  YOUR_PHONE: "+1 803 380 4145",
+  description: "Please let me know your demand",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/aoeu256",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/david-kuczynski-a818972bb/",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
